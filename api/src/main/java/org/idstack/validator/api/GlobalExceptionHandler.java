f8211b2402953ddddfd6104cf5c55134fa41857a
Chanaka Lakmal <ldclakmal@gmail.com>
2017-08-27 13:49:39 +0530

    Implement simple exception handler

---
+++ api/src/main/java/org/idstack/validator/api/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package org.idstack.validator.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author Chanaka Lakmal
+ * @date 27/8/2017
+ * @since 1.0
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(RuntimeException.class)
+    public String  handle(RuntimeException e) {
+        return e.getMessage();
+    }
+}
