83891d2bba70c06a7a05ff77794af1f163221f3e
ldclakmal <ldclakmal@gmail.com>
2017-08-31 20:40:53 +0530

    Fix a bug

--- api/src/main/java/org/idstack/validator/api/Router.java
+++ api/src/main/java/org/idstack/validator/api/Router.java
@@ -18,43 +18,43 @@ import java.util.Properties;
  */
 public class Router {
 
-    public final String src = getProperty(Constant.GlobalAttribute.CONFIG_FILE_PATH);
+    public final String configFilePath = getProperty(Constant.GlobalAttribute.CONFIG_FILE_PATH);
 
     public String saveBasicConfiguration(String json) {
-        return FeatureImpl.getFactory().saveBasicConfiguration(src, json);
+        return FeatureImpl.getFactory().saveBasicConfiguration(configFilePath, json);
     }
 
     public String saveDocumentConfiguration(String json) {
-        return FeatureImpl.getFactory().saveDocumentConfiguration(src, json);
+        return FeatureImpl.getFactory().saveDocumentConfiguration(configFilePath, json);
     }
 
     public String saveWhiteListConfiguration(String json) {
-        return FeatureImpl.getFactory().saveWhiteListConfiguration(src, json);
+        return FeatureImpl.getFactory().saveWhiteListConfiguration(configFilePath, json);
     }
 
     public String saveBlackListConfiguration(String json) {
-        return FeatureImpl.getFactory().saveBlackListConfiguration(src, json);
+        return FeatureImpl.getFactory().saveBlackListConfiguration(configFilePath, json);
     }
 
     public Object getConfiguration(String type, String property) {
         String src = null;
         switch (type) {
-            case "basic":
+            case Constant.GlobalAttribute.BASIC_CONFIG:
                 src = Constant.GlobalAttribute.BASIC_CONFIG_FILE_NAME;
                 break;
-            case "document":
+            case Constant.GlobalAttribute.DOCUMENT_CONFIG:
                 src = Constant.GlobalAttribute.DOCUMENT_CONFIG_FILE_NAME;
                 break;
-            case "whitelist":
+            case Constant.GlobalAttribute.WHITELIST_CONFIG:
                 src = Constant.GlobalAttribute.WHITELIST_CONFIG_FILE_NAME;
                 break;
-            case "blacklist":
+            case Constant.GlobalAttribute.BLACKLIST_CONFIG:
                 src = Constant.GlobalAttribute.BLACKLIST_CONFIG_FILE_NAME;
                 break;
             default:
                 break;
         }
-        return FeatureImpl.getFactory().getConfiguration(src, property);
+        return FeatureImpl.getFactory().getConfiguration(configFilePath + src, property);
     }
 
     public String saveCertificate(String category, MultipartFile certificate, String password) {
@@ -76,7 +76,7 @@ public class Router {
                 break;
         }
 
-        File file = FeatureImpl.getFactory().saveCertificate(src, type, flag, password, getProperty(Constant.GlobalAttribute.PVT_CERTIFICATE_PASSWORD_TYPE));
+        File file = FeatureImpl.getFactory().saveCertificate(configFilePath, src, type, flag, password, getProperty(Constant.GlobalAttribute.PVT_CERTIFICATE_PASSWORD_TYPE));
 
         try {
             certificate.transferTo(file);
@@ -87,11 +87,11 @@ public class Router {
     }
 
     public FileSystemResource getPublicCertificate(String uuid) {
-        return new FileSystemResource(new File(getProperty(Constant.GlobalAttribute.PUB_CERTIFICATE_FILE_PATH) + getProperty(Constant.GlobalAttribute.PUB_CERTIFICATE_TYPE) + uuid));
+        return new FileSystemResource(new File(getProperty(Constant.GlobalAttribute.PUB_CERTIFICATE_FILE_PATH) + uuid + getProperty(Constant.GlobalAttribute.PUB_CERTIFICATE_TYPE)));
     }
 
     public String signDocument(String json) {
-        String output = FeatureImpl.getFactory().signDocument(json);
+        String output = FeatureImpl.getFactory().signDocument(configFilePath, json);
         if (output.equals(Constant.Status.OK)) {
             // TODO
             // call sign method(document, isContentSignable, urlList, getPrivateCertificate(), getPassword())
