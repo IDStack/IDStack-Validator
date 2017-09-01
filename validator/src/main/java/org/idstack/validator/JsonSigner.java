d6cd53232c5190537131e7933218228b38b4b693
ldclakmal <ldclakmal@gmail.com>
2017-09-01 17:45:33 +0530

    Add validator classes

---
+++ validator/src/main/java/org/idstack/validator/JsonSigner.java
@@ -0,0 +1,130 @@
+package org.idstack.validator;
+
+import com.google.gson.Gson;
+import org.bouncycastle.cert.jcajce.JcaCertStore;
+import org.bouncycastle.cms.*;
+import org.bouncycastle.cms.jcajce.JcaSignerInfoGeneratorBuilder;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.operator.jcajce.JcaDigestCalculatorProviderBuilder;
+import org.bouncycastle.util.Store;
+import org.bouncycastle.util.encoders.Base64;
+import org.idstack.feature.Parser;
+import org.idstack.feature.document.Document;
+import org.idstack.feature.document.Signature;
+import org.idstack.feature.document.Validator;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author Chandu Herath
+ * @date 26/05/2017
+ * @since 1.0
+ */
+
+public class JsonSigner {
+
+    public static final String SIGNATUREALGO = "SHA256withRSA";
+    public static final String PKCS12FILE = "BuddhikaWijebandara.pfx";
+    public static final char[] PKCS12PASSWORD = "abc123".toCharArray();
+    public static final String PUBLICCERURL = "idstack/public_certificates/BuddhikaWijebandara.cer";
+
+
+    private Parser jsonParser = new Parser();
+    private SignPreProcessor signPreProcessor = new SignPreProcessor();
+
+    public String signJson(String jsonString, boolean signContent, ArrayList<String> signList) throws CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, OperatorCreationException, CMSException, IOException, CloneNotSupportedException, NoSuchProviderException {
+
+        Document digitizedDocument = jsonParser.parseDocumentJson(jsonString);
+        Document signOnlyDigitizedDocument = jsonParser.parseDocumentJson(jsonString);
+
+        signOnlyDigitizedDocument = signPreProcessor.getSignOnlyDigitizedVersion(signOnlyDigitizedDocument, signContent, signList);
+        digitizedDocument = sign(signOnlyDigitizedDocument, digitizedDocument, signContent, signList);
+
+        return new Gson().toJson(digitizedDocument);
+    }
+
+    private Document sign(Document signOnlyDigitalJson, Document completeDigitalJson, boolean signContent, ArrayList<String> signList) throws CertificateException,
+            NoSuchAlgorithmException, KeyStoreException, IOException, UnrecoverableKeyException, CMSException,
+            OperatorCreationException, NoSuchProviderException {
+
+        String jsonStringToSign = new Gson().toJson(signOnlyDigitalJson);
+
+        //Setting BouncyCastle as the security provoder
+        BouncyCastleProvider provider = new BouncyCastleProvider();
+        Security.addProvider(provider);
+
+        //Load the .pfx file into a key store
+        KeyStore keyStore = this.loadKeyStore(provider);
+
+        CMSSignedDataGenerator signatureGenerator = this.setUpProvider(keyStore);
+        byte[] signedBytes = this.signMessagePKCS7(jsonStringToSign.getBytes("UTF-8"), signatureGenerator);
+        String signedString = new String(Base64.encode(signedBytes));
+
+        String signerID = generateUniqueID();//generate signature ID
+        Signature signature = new Signature(signedString, PUBLICCERURL);
+        if (signList == null) {
+            signList = new ArrayList<>();
+        }
+        Validator newSigner = new Validator(signerID, signature, signContent, signList);
+
+        completeDigitalJson.getValidators().add(newSigner);
+
+        return completeDigitalJson;
+    }
+
+    private KeyStore loadKeyStore(BouncyCastleProvider provider) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, NoSuchProviderException {
+        KeyStore ks = KeyStore.getInstance("PKCS12", provider.getName());
+        ks.load(new FileInputStream(getClass().getClassLoader().getResource(PKCS12FILE).getFile()), PKCS12PASSWORD);
+        return ks;
+    }
+
+    private CMSSignedDataGenerator setUpProvider(final KeyStore keyStore) throws KeyStoreException, CertificateEncodingException,
+            UnrecoverableKeyException, NoSuchAlgorithmException, OperatorCreationException, CMSException {
+        String alias = keyStore.aliases().nextElement();
+        java.security.cert.Certificate[] certChain = keyStore.getCertificateChain(alias);
+
+        final List<java.security.cert.Certificate> certList = new ArrayList<>();
+        for (int i = 0, length = certChain == null ? 0 : certChain.length; i < length; i++) {
+            certList.add(certChain[i]);
+        }
+
+        Store certStore = new JcaCertStore(certList);//Storing certificates for later lookups
+
+        java.security.cert.Certificate cert = keyStore.getCertificate(alias);
+
+        ContentSigner signer = new JcaContentSignerBuilder(SIGNATUREALGO).setProvider("BC").build((PrivateKey) keyStore.
+                getKey(alias, PKCS12PASSWORD));
+        CMSSignedDataGenerator generator = new CMSSignedDataGenerator();
+        generator.addSignerInfoGenerator(new JcaSignerInfoGeneratorBuilder(new JcaDigestCalculatorProviderBuilder().
+                setProvider("BC").build()).build(signer, (X509Certificate) cert));
+        generator.addCertificates(certStore);
+
+        return generator;
+
+    }
+
+    private byte[] signMessagePKCS7(final byte[] content, final CMSSignedDataGenerator generator) throws CMSException,
+            IOException {
+        CMSTypedData cmsdata = new CMSProcessableByteArray(content);
+        CMSSignedData signeddata = generator.generate(cmsdata, true);
+        return signeddata.getEncoded();
+    }
+
+    private String generateUniqueID() {
+        String randomID = UUID.randomUUID().toString();
+        String[] idArray = randomID.split("-");
+        return idArray[0];
+    }
+
+}
---
+++ validator/src/main/java/org/idstack/validator/SignPreProcessor.java
@@ -0,0 +1,47 @@
+package org.idstack.validator;
+
+import org.idstack.feature.document.Document;
+import org.idstack.feature.document.Validator;
+
+import java.util.ArrayList;
+
+/**
+ * @author Chandu Herath
+ * @date 30/05/2017
+ * @since 1.0
+ */
+
+public class SignPreProcessor {
+
+    public Document getSignOnlyDigitizedVersion(Document wholeVersion, boolean signContent, ArrayList<String> signList) throws CloneNotSupportedException {
+        //removing content if it is not supposed to be signed
+        if (!signContent) {
+            wholeVersion.getContent().clear();
+        }
+
+        //removing signatures witch are not supposed to be signed
+        if (signList != null) {
+            ArrayList<String> signerIDList = new ArrayList<>();
+            for (int i = 0; i < wholeVersion.getValidators().size(); i++) {
+                signerIDList.add(wholeVersion.getValidators().get(i).getId());
+            }
+
+            ArrayList<Validator> signerList = new ArrayList();
+
+            for (int i = 0; i < signerIDList.size(); i++) {
+                String temp = signerIDList.get(i);
+                for (int j = 0; j < signList.size(); j++) {
+                    if (signList.get(j).equals(temp)) {
+                        signerList.add(wholeVersion.getValidators().get(i));
+                    }
+                }
+            }
+            wholeVersion.setValidators(signerList);
+        } else {
+            wholeVersion.getValidators().clear();
+        }
+
+        return wholeVersion;
+    }
+
+}
