0a0c7c34ef4c6ccddf8022bceb5b524e69f365ea
ldclakmal <ldclakmal@gmail.com>
2017-09-20 23:41:25 +0530

    Document the api

--- api/src/main/java/org/idstack/validator/api/APIHandler.java
+++ api/src/main/java/org/idstack/validator/api/APIHandler.java
@@ -28,6 +28,15 @@ public class APIHandler {
         httpServletResponse.sendRedirect("http://docs.idstack.apiary.io/");
     }
 
+    /**
+     * Save the configurations received at the configured URL at idstack.properties file
+     *
+     * @param version api version
+     * @param apikey  api key
+     * @param type    type of configuration [basic, document, whitelist, blacklist]
+     * @param json    json of configuration
+     * @return status of saving
+     */
     @RequestMapping(value = "/{version}/{apikey}/saveconfig/{type}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public String saveConfiguration(@PathVariable("version") String version, @PathVariable("apikey") String apikey, @PathVariable("type") String type, @RequestBody String json) {
@@ -49,9 +58,18 @@ public class APIHandler {
         }
     }
 
+    /**
+     * Returned the saved configurations. If property is mentioned this returns only the mentioned property from the given type otherwise everything
+     *
+     * @param version  api version
+     * @param apikey   api key
+     * @param type     type of configuration [basic, document, whitelist, blacklist]
+     * @param property property of configuration
+     * @return configuration as json
+     */
     @RequestMapping(value = {"/{version}/{apikey}/getconfig/{type}/{property}", "/{version}/{apikey}/getconfig/{type}"}, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public String getConfigurationFile(@PathVariable("version") String version, @PathVariable("apikey") String apikey, @PathVariable("type") String type, @PathVariable("property") Optional<String> property) {
+    public String getConfiguration(@PathVariable("version") String version, @PathVariable("apikey") String apikey, @PathVariable("type") String type, @PathVariable("property") Optional<String> property) {
         if (!FeatureImpl.getFactory().validateRequest(version))
             return Constant.Status.STATUS_ERROR_VERSION;
         if (!FeatureImpl.getFactory().validateRequest(router.apiKey, apikey))
@@ -59,6 +77,14 @@ public class APIHandler {
         return FeatureImpl.getFactory().getConfigurationAsJson(router.configFilePath, router.getConfigFileName(type), property);
     }
 
+    /**
+     * Save public certificate of the validator
+     *
+     * @param version     api version
+     * @param apikey      api key
+     * @param certificate public certificate file
+     * @return status of saving
+     */
     @RequestMapping(value = "/{version}/{apikey}/savepubcert", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseBody
     public String savePublicCertificate(@PathVariable("version") String version, @PathVariable("apikey") String apikey, @RequestParam(value = "cert") final MultipartFile certificate) {
@@ -69,6 +95,12 @@ public class APIHandler {
         return FeatureImpl.getFactory().savePublicCertificate(certificate, router.configFilePath, router.pubCertFilePath, router.pubCertType);
     }
 
+    /**
+     * Get public certificate of the validator
+     *
+     * @param version api version
+     * @return URL of the public certificate
+     */
     @RequestMapping(value = "/{version}/getpubcert", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody
     public String getPublicCertificate(@PathVariable("version") String version) {
@@ -77,6 +109,15 @@ public class APIHandler {
         return FeatureImpl.getFactory().getPublicCertificateURL(router.configFilePath, router.pubCertFilePath, router.pubCertType);
     }
 
+    /**
+     * Save private certificate of the validator
+     *
+     * @param version     api version
+     * @param apikey      api key
+     * @param certificate private certificate
+     * @param password    password of private certificate
+     * @return status of saving
+     */
     @RequestMapping(value = "/{version}/{apikey}/savepvtcert", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseBody
     public String savePrivateCertificate(@PathVariable("version") String version, @PathVariable("apikey") String apikey, @RequestParam(value = "cert") final MultipartFile certificate, @RequestParam(value = "password") String password) {
@@ -88,6 +129,17 @@ public class APIHandler {
     }
 
     //Access by the owner
+
+    /**
+     * Automatically sign the received json document + pdf document and return the signed documents
+     *
+     * @param version api version
+     * @param json    json document
+     * @param pdf     pdf document
+     * @param email   email of the sender
+     * @return signed json + pdf documents
+     * @throws IOException if file cannot be converted into bytes
+     */
     //TODO : return both signed MR + signed PDF
     @RequestMapping(value = "/{version}/sign", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
@@ -97,6 +149,15 @@ public class APIHandler {
         return router.signDocumentAutomatically(json, pdf, email);
     }
 
+    /**
+     * Sign the received json document + pdf document by the validator and return the signed documents
+     *
+     * @param version api version
+     * @param json    json document
+     * @param pdf     pdf document
+     * @return signed json + pdf documents
+     * @throws IOException if file cannot be converted into bytes
+     */
     //TODO : return both signed MR + signed PDF
     @RequestMapping(value = "/{version}/{apikey}/sign", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
@@ -106,6 +167,13 @@ public class APIHandler {
         return router.signDocumentManually(json, pdf);
     }
 
+    /**
+     * Get the document types that protocol facilitates
+     *
+     * @param version api version
+     * @param apikey  api key
+     * @return document types
+     */
     //TODO : pass this to lambda function
     @RequestMapping(value = "/{version}/{apikey}/getdoctypes", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
@@ -117,11 +185,20 @@ public class APIHandler {
         return FeatureImpl.getFactory().getDocumentTypes();
     }
 
+    /**
+     * Get the stored documents in the configured store path
+     *
+     * @param version api version
+     * @param apikey  api key
+     * @return document list
+     */
     @RequestMapping(value = "/{version}/{apikey}/getdocstore", method = RequestMethod.GET)
     @ResponseBody
-    public String getStoredDocuments(@PathVariable("version") String version) {
+    public String getStoredDocuments(@PathVariable("version") String version, @PathVariable("apikey") String apikey) {
         if (!FeatureImpl.getFactory().validateRequest(version))
             return Constant.Status.STATUS_ERROR_VERSION;
+        if (!FeatureImpl.getFactory().validateRequest(router.apiKey, apikey))
+            return Constant.Status.STATUS_ERROR_API_KEY;
         return FeatureImpl.getFactory().getDocumentStore(router.storeFilePath);
     }
 }
