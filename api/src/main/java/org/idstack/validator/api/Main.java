84b6969657edba288d9e7a431903c7a2ea04c0cb
ldclakmal <ldclakmal@gmail.com>
2017-09-21 23:54:33 +0530

    Fix a bug

--- api/src/main/java/org/idstack/validator/api/Main.java
+++ api/src/main/java/org/idstack/validator/api/Main.java
@@ -14,7 +14,8 @@ import org.springframework.context.annotation.Configuration;
  */
 
 @Configuration
-@ComponentScan
+@ComponentScan({"org.idstack.validator"})
+@ComponentScan({"org.idstack.feature"})
 @EnableAutoConfiguration
 public class Main extends SpringBootServletInitializer {
 
