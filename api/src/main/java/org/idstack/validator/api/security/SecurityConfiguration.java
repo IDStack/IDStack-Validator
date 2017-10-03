8ab8c1ab66dfe017f6ea90360684a953f0d57f41
ldclakmal <ldclakmal@gmail.com>
2017-10-03 20:31:26 +0530

    Add security module for api

---
+++ api/src/main/java/org/idstack/validator/api/security/SecurityConfiguration.java
@@ -0,0 +1,63 @@
+package org.idstack.validator.api.security;
+
+import org.idstack.feature.Constant;
+import org.idstack.feature.FeatureImpl;
+import org.idstack.validator.api.Router;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    Router router;
+
+    @Autowired
+    private FeatureImpl feature;
+
+    @Value(value = "classpath:" + Constant.Configuration.SYSTEM_PROPERTIES_FILE_NAME)
+    private Resource resource;
+
+    private String apiKey;
+    private String username;
+    private String password;
+
+    @Autowired
+    void init() throws IOException {
+        apiKey = feature.getProperty(resource.getInputStream(), Constant.Configuration.API_KEY);
+        username = feature.getProperty(resource.getInputStream(), Constant.Configuration.USERNAME);
+        password = feature.getProperty(resource.getInputStream(), Constant.Configuration.PASSWORD);
+    }
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser(username).password(password).roles(Constant.Configuration.USER_ADMIN);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/*/" + apiKey + "/**").hasRole(Constant.Configuration.USER_ADMIN)
+                .and().httpBasic()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//We don't need sessions to be created.
+    }
+
+    /* To allow Pre-flight [OPTIONS] request from browser */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+    }
+}
