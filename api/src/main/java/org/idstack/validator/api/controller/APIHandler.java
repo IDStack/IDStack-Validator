e7ccacd6cbada5301bddd7112f473dea39db26d4
ldclakmal <ldclakmal@gmail.com>
2017-10-10 12:10:11 +0530

    Refactor for api changes

--- api/src/main/java/org/idstack/validator/api/controller/APIHandler.java
+++ api/src/main/java/org/idstack/validator/api/controller/APIHandler.java
@@ -196,7 +196,7 @@ public class APIHandler {
             return new Gson().toJson(Collections.singletonMap(Constant.Status.STATUS, Constant.Status.ERROR_VERSION));
         if (!feature.validateRequest(apiKey, apikey))
             return new Gson().toJson(Collections.singletonMap(Constant.Status.STATUS, Constant.Status.ERROR_API_KEY));
-        return feature.getDocumentStore(storeFilePath);
+        return feature.getDocumentStore(storeFilePath, true);
     }
 
     //*************************************************** PUBLIC API ***************************************************
